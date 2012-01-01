ec5540e9331c1ae79c5f41697cb8984d603859e0
Will Wen Gunn <i@iwillwen.com>
2012-01-01 17:23:50 +0800

    v0.3.9 Faster router

---
+++ examples/play.js
@@ -0,0 +1,22 @@
+var http=require('http');
+
+var options = {
+  host: 'www.google.com',
+  port: 80,
+  path: '',
+  method: 'GET'
+};
+
+var req = http.request(options, function(res) {
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
+  });
+});
+
+req.on('error', function(e) {
+  console.log('problem with request: ' + e.message);
+});
+req.end();
